ce730097aa9db91be9925be801848b0b0b9313ed
bekkkks <ewefe811@gmail.com>
2022-10-30 18:52:22 +0600

    less8

--- lessons/lesson 8.py
+++ lessons/lesson 8.py
@@ -21,9 +21,21 @@ def create_table(cann, sql):
         print(err)
 
 
+def create_student(cann, student):
+    try:
+        sql = ''' INSERT INTO students
+        (fullname,mark,hobby,birth_date,is_married)
+        VALUES(?,?,?,?,?)'''
+        cursor = cann.cursor()
+        cursor.execute(sql, student)
+        cann.commit()
+    except Error as err:
+        print(err)
+
+
 create_student_table = '''
 CREATE TABLE students(
-id INTEGER PRIMARY KEY,
+id INTEGER PRIMARY KEY AUTOINCREMENT,
 fullname VARCHAR (100) NOT NULL,
 mark DOUBLE (5,2) NOT NULL DEFAULT 0.0,
 hobby TEXT DEFAULT NULL,
@@ -35,5 +47,12 @@ connekt = create_connektion(r'g22_3_3.db')
 
 if connekt is not None:
     print('все отлично')
-    create_table(connekt, create_student_table)
+    # create_table(connekt, create_student_table)
+    create_student(connekt, ("Mark Daniels", 77.12, "Football", "1999-01-02", False))
+    create_student(connekt, ("Alex Brilliant", 77.12, None, "1989-12-31", True))
+    create_student(connekt, ("Diana Julls", 99.3, "Tennis", "2005-01-22", True))
+    create_student(connekt, ("Michael Corse", 100.0, "Diving", "2001-09-17", True))
+    create_student(connekt, ("Jack Moris", 50.2, "Fishing and cooking", "2001-07-12", True))
+    create_student(connekt, ("Viola Manilson", 41.82, None, "1991-03-01", False))
+    create_student(connekt, ('beka dz', 44.44, 'стихи', '2003-06-06', False))
     print('код выполнен')
